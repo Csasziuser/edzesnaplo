487d180eab380e2a53818e6c1cdde489dbf6625d
Császár Martin Krisztián <145013640+Csasziuser@users.noreply.github.com>
2025-02-07 10:33:05 +0100

    2-3-4. Feladat(Ábel)

---
+++ app/Http/Controllers/BejegyzesController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class BejegyzesController extends Controller
+{
+    function index() {
+
+    }
+
+    function store() {
+
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,10 +1,15 @@
 <?php
 
+use App\Http\Controllers\BejegyzesController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+Route::get('/edzesnaplo', [BejegyzesController::class, 'index'])->name('bejegyzes.index');
+Route::post('/edzesnaplo', [BejegyzesController::class, 'store'])->name('bejegyzes.store');
+
 
 
