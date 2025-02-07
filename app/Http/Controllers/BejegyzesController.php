31ee282a9cd84a1973262a5624dcf01d3e43eb92
Császár Martin Krisztián <145013640+Csasziuser@users.noreply.github.com>
2025-02-07 11:22:03 +0100

    Attila

--- app/Http/Controllers/BejegyzesController.php
+++ app/Http/Controllers/BejegyzesController.php
@@ -2,7 +2,10 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\bejegyzes;
+use Illuminate\Validation\ValidationException;
 use Illuminate\Http\Request;
+use Log;
 
 class BejegyzesController extends Controller
 {
@@ -10,7 +13,24 @@ class BejegyzesController extends Controller
 
     }
 
-    function store() {
+    function store(Request $request) {
+        try {
+            $request->validate([
+                'gyakorlat'=>'required|string',
+                'ismetlesszam'=>'required|min:1|max:8|integer'
+            ],[
+                'min'=>'Érvénytelen ismétlésszám!'
+            ]);
 
+        } catch (ValidationException $th) {
+            //->header('Access-Control-Allow-Origin','*')
+            return response()->json(['success'=>false,'message'=>$th->errors()],201,['Access-Control-Allow-Origin'=>'*'],JSON_UNESCAPED_UNICODE);
+        }
+        bejegyzes::create([
+            'gyakorlat'=>$request->gyakorlat,
+            'ismetlesszam'=>$request->ismetlesszam,
+        ]);
+        return response()->json(['success'=>true,'message'=>'Rekord sikeresen hozzáadva!'],201,['Access-Control-Allow-Origin'=>'*'],JSON_UNESCAPED_UNICODE);
+    
     }
 }
